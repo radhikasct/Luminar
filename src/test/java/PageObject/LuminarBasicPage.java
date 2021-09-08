d8224eab6c81e651e9dcc2aa9a02fc9461543876
radhikasct@gmail.com <radhikasct@gmail.com>
2021-09-09 01:44:05 +0530

    Second Commit

--- src/test/java/PageObject/LuminarBasicPage.java
+++ src/test/java/PageObject/LuminarBasicPage.java
@@ -34,28 +34,7 @@ public class LuminarBasicPage extends BaseUtil{
 	@FindBy(css = "div.layout-wide-content.layout-wide-column-right5 > div > div > div > input")
 	WebElement getBuy1;
 	
-	@FindBy(xpath = "//*[@id='root']/div/div[2]/div[2]/div/div/div[3]/div/div/div[1]/div/div[1]/div/div[1]/div/input")
-	WebElement enterSell2;
 
-	@FindBy(xpath = "//*[@id='root']/div/div[2]/div[2]/div/div/div[3]/div/div/div[1]/div/div[1]/div/div[1]/span/div/span/div/span")
-	WebElement clickEUR2;
-	
-	@FindBy(xpath = "/html/body/div[4]/div/div/div/div/div/div[2]/button[14]")
-	WebElement selUSD2;
-
-	@FindBy(xpath = "//*[@id='root']/div/div[2]/div[2]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div[1]/div/input")
-	WebElement getBuy2;
-	//*[@id="root"]/div/div[2]/div[2]/div/div/div[3]/div/div/div[1]/div/div[1]/div/div[1]/div/input
-	
-	
-	
-	By enterSell = By.cssSelector("div.layout-wide-content.layout-wide-column-left5 > div > div> div > input");
-	
-	By clickEUR = By.cssSelector("div.layout-wide-content.layout-wide-column-left5 > div > div> span > div > span > div > span");
-	
-	By selUSD = By.cssSelector("div.currency-select-menu>button:last-of-type");
-	
-	By getBuy = By.cssSelector("div.layout-wide-content.layout-wide-column-right5 > div > div > div > input");
 	
 	public void getBuyValue() {
 		try {
@@ -80,26 +59,5 @@ public class LuminarBasicPage extends BaseUtil{
 			captureScreen(driver);
 		}
 	}
-	public void getBuyValuexpath() {
-		try {
-			Thread.sleep(5000);
-		}catch(Exception e) {
-			System.out.println("Error When conversion "+e.getStackTrace());
-			captureScreen(driver);
-		}
-		//captureScreen(wdriver);
-		(enterSell2).click();
-			(enterSell2).sendKeys("100");
-			(clickEUR2).click();
-			(selUSD2).click();
-			String buyValue = (getBuy2).getText();
-			System.out.println(buyValue);
-			captureScreen(driver);
-			
-		
-	}
 	
-	
-	
-
 }
